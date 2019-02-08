444a7a68003ea29ffa336121489fdb2f9f064ddb
mteodor <mirko.teodorovic@gmail.com>
2019-02-08 16:41:19 +0100

    Saving my work, just in case

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/mainflux/mainflux"
 	"github.com/mainflux/mainflux/logger"
 	"github.com/mteodor/edgex-app/exapp"
+	"github.com/mteodor/edgex-app/exapp/postgres"
 	nats "github.com/nats-io/go-nats"
 )
 
 const (
-	defNatsURL  string = nats.DefaultURL
-	defLogLevel string = "error"
-	defPort     string = "8000"
-	envNatsURL  string = "MF_NATS_URL"
-	envLogLevel string = "MF_EDGEX_APP_LOG_LEVEL"
-	envPort     string = "MF_EDGEX_APP_PORT"
+	defNatsURL       = nats.DefaultURL
+	defLogLevel      = "error"
+	defPort          = "8000"
+	envNatsURL       = "MF_NATS_URL"
+	envLogLevel      = "MF_EDGEX_APP_LOG_LEVEL"
+	envPort          = "MF_EDGEX_APP_PORT"
+	defDBHost        = "localhost"
+	defDBPort        = "5432"
+	defDBUser        = "mainflux"
+	defDBPass        = "mainflux"
+	defDBName        = "events"
+	defDBSSLMode     = "disable"
+	defDBSSLCert     = ""
+	defDBSSLKey      = ""
+	defDBSSLRootCert = ""
+	envDBHost        = "MF_EDGEX_DB_HOST"
+	envDBPort        = "MF_EDGEX_DB_PORT"
+	envDBUser        = "MF_EDGEX_DB_USER"
+	envDBPass        = "MF_EDGEX_DB_PASS"
+	envDBName        = "MF_EDGEX_DB"
+	envDBSSLMode     = "MF_EDGEX_DB_SSL_MODE"
+	envDBSSLCert     = "MF_EDGEX_DB_SSL_CERT"
+	envDBSSLKey      = "MF_EDGEX_DB_SSL_KEY"
+	envDBSSLRootCert = "MF_EDGEX_DB_SSL_ROOT_CERT"
 )
 
 type config struct {
+	dbConfig postgres.Config
 	NatsURL  string
 	LogLevel string
 	Port     string
@@ -39,23 +58,25 @@ func main() {
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
-	// Connect to a server
+	// Connect to a NATS server
 
-	logger.Info(fmt.Sprintf("connecting %s\n", defNatsURL))
+	db := connectToDB(cfg.dbConfig, logger)
+	defer db.Close()
+
+	logger.Info(fmt.Sprintf("connecting %s", defNatsURL))
 	nc, err := nats.Connect(defNatsURL)
 	if err != nil {
-		logger.Error("Failed to connect\n")
+		logger.Error("Failed to connect")
 	}
 	defer closeConn(nc)
 	// Simple Async Subscriber
-	nc.Subscribe("out.unknown", exapp.MakeHandler())
+	nc.Subscribe("out.unknown", exapp.NatsMSGHandler())
 
 	errs := make(chan error, 2)
 
 	go func() {
-		p := fmt.Sprintf(":%s", cfg.Port)
 		logger.Info(fmt.Sprintf("edgex-app started, exposed port %s", cfg.Port))
-		errs <- http.ListenAndServe(p, exapp.MakeHTTPHandler())
+		errs <- exapp.InitHTTP(cfg.Port)
 	}()
 
 	go func() {
@@ -82,9 +103,42 @@ func closeConn(nc *nats.Conn) {
 }
 
 func loadConfig() config {
-	return config{
-		NatsURL:  mainflux.Env(envNatsURL, defNatsURL),
-		LogLevel: mainflux.Env(envLogLevel, defLogLevel),
-		Port:     mainflux.Env(envPort, defPort),
+
+	dbConfig := postgres.Config{
+		Host:        Env(envDBHost, defDBHost),
+		Port:        Env(envDBPort, defDBPort),
+		User:        Env(envDBUser, defDBUser),
+		Pass:        Env(envDBPass, defDBPass),
+		Name:        Env(envDBName, defDBName),
+		SSLMode:     Env(envDBSSLMode, defDBSSLMode),
+		SSLCert:     Env(envDBSSLCert, defDBSSLCert),
+		SSLKey:      Env(envDBSSLKey, defDBSSLKey),
+		SSLRootCert: Env(envDBSSLRootCert, defDBSSLRootCert),
 	}
+
+	return config{
+		NatsURL:  Env(envNatsURL, defNatsURL),
+		LogLevel: Env(envLogLevel, defLogLevel),
+		Port:     Env(envPort, defPort),
+		dbConfig: dbConfig,
+	}
+}
+
+func connectToDB(dbConfig postgres.Config, logger logger.Logger) *sql.DB {
+	db, err := postgres.Connect(dbConfig)
+
+	if err != nil {
+		logger.Error(fmt.Sprintf("Failed to connect to postgres: %s", err))
+		os.Exit(1)
+	}
+	return db
+}
+
+//geting enviroment
+func Env(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+
+	return fallback
 }
--- exapp/exapp.go
+++ exapp/exapp.go
@@ -1,36 +1,5 @@
 package exapp
 
-import (
-	"encoding/json"
-	"fmt"
-	"html"
-	"net/http"
-
-	model "github.com/edgexfoundry/edgex-go/pkg/models"
-	nats "github.com/nats-io/go-nats"
-)
-
-// MakeHandler returns a HTTP handler for API endpoints.
-func MakeHandler() nats.MsgHandler {
-
-	return func(msg *nats.Msg) {
-		fmt.Printf("Received a message: %s\n", string(msg.Data))
-		processMsg(msg)
-	}
-}
-func MakeHTTPHandler() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		// If request is GET with correct syntax
-		fmt.Fprintf(w, "%q", html.EscapeString("edgex-app iw working\n"))
-
-	})
-
-}
-func processMsg(msg *nats.Msg) {
-	data := new(model.Event)
-	json.Unmarshal(msg.Data, &data)
-	fmt.Printf("unmarshalled:" + data.String())
-
+func Validate() error {
+	return nil
 }
