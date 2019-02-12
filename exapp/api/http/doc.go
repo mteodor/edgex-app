145bc69ce4ef534106af24780cfd2cae7f466f64
mteodor <mirko.teodorovic@gmail.com>
2019-02-12 15:09:49 +0100

    Reorganize code

---
+++ exapp/api/http/doc.go
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2018
+// Mainflux
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+// Package http contains implementation of edgex-app service HTTP API.
+
+package http
---
+++ exapp/service.go
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2018
+// Mainflux
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package events
+
+import (
+	model "github.com/edgexfoundry/edgex-go/pkg/models"
+)
+
+// Service specifies an API that must be fullfiled by the domain service
+// implementation, and all of its decorators (e.g. logging & metrics).
+type Service interface {
+	// Register adds new evetn
+	RegisterEvent(model.Event) error
+
+	RetrieveByID(string) (model.Event, error)
+
+}
+
+var _ Service = (*eventsService)(nil)
+
+type eventsService struct {
+	events EventsRepository
+	//	hasher Hasher
+	//	idp    IdentityProvider
+}
+
+// New instantiates the events service implementation.
+func New(events EventsRepository) Service {
+	return &eventsService{events: events}
+}
+
+func (svc eventsService) RegisterEvent(event model.Event) error {
+
+	return svc.events.Save(event)
+}
+
+
+func (svc eventsService) RetrieveByID(id string) (model.Event, error) {
+	return svc.events.RetrieveByID(id)
+
+}
+
