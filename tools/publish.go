58906b6316af1b9e425ea85f65de8d87e2d2839b
mteodor <mirko.teodorovic@gmail.com>
2019-02-14 16:03:10 +0100

    make string in multiple line Signed-off-by: mteodor <mirko.teodorovic@gmail.com>

--- tools/publish.go
+++ tools/publish.go
@@ -8,7 +8,25 @@ import (
 
 const (
 	topicUnknown = "out.unknown"
-	testMessage  = "{\"id\":\"5c59ee1c0e360800016fc255\",\"pushed\":0,\"device\":\"Random-Integer-Generator01\",\"created\":1549397532000,\"modified\":0,\"origin\":1549397532000,\"schedule\":null,\"event\":null,\"readings\":[{\"id\":\"5c59ee1c0e360800016fc256\",\"pushed\":0,\"created\":1549397532000,\"origin\":1549397532000,\"modified\":0,\"device\":\"Random-Integer-Generator01\",\"name\":\"RandomValue_Int32\",\"value\":\"116177668\"}]}"
+	testMessage  = `{	\"id\":\"5c59ee1c0e360800016fc255\",
+						\"pushed\":0,
+						\"device\":\"Random-Integer-Generator01\",
+						\"created\":1549397532000,
+						\"modified\":0,
+						\"origin\":1549397532000,
+						\"schedule\":null,
+						\"event\":null,
+						\"readings\":
+							[{	\"id\":\"5c59ee1c0e360800016fc256\",
+								\"pushed\":0,
+								\"created\":1549397532000,
+								\"origin\":1549397532000,
+								\"modified\":0,
+								\"device\":\"Random-Integer-Generator01\",
+								\"name\":\"RandomValue_Int32\",
+								\"value\":\"116177668\"
+							}]
+					}`
 )
 
 func main() {
